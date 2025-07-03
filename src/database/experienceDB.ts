8bf8915e16cd2daaa6cd14d8bafcdb66df8239a3
chaicopadillag <xt9738@rimac.com.pe>
2025-07-03 11:34:29 -0500

    feat: Actualiza CV y experiencia laboral

--- src/database/experienceDB.ts
+++ src/database/experienceDB.ts
@@ -2,21 +2,38 @@ import { ExperienceType } from '../types/ExperienceType';
 
 export const experienceDB: ExperienceType[] = [
   {
+    id: 'a2820888-c7d8-4392-a8f5-75b515de6ebc',
+    position: 'Backend Developer',
+    company: 'Indra (Rimac)',
+    location: 'Lima',
+    startDate: 'Octubre, 2024',
+    endDate: 'Presente',
+    achievements: [
+      'Diseño e implementación de soluciones backend serverless compuestas por AWS Lambda, API Gateway y DynamoDB, conformando microservicios desacoplados y escalables.',
+      'Construcción de flujos orquestados mediante AWS Step Functions, integrando servicios como Lambda, SNS, DynamoDB y EventBridge para procesos tanto síncronos como asíncronos.',
+      'Gestión de la seguridad en APIs mediante AWS Cognito (autenticación y autorización), API Keys y AWS Signature V4, cumpliendo con buenas prácticas de protección de endpoints.',
+      'Desarrollo de pruebas automatizadas con Jest y Jest-Cucumber, aplicando metodologías de TDD y BDD para asegurar la calidad y mantenibilidad del código.',
+      'Uso de herramientas de observabilidad y logging como AWS CloudWatch y AWS X-Ray para monitoreo, trazabilidad y resolución de incidencias.',
+      'Aplicación de prácticas DevSecOps y despliegues automatizados mediante pipelines CI/CD utilizando servicios como Azure DevOps.'
+    ],
+    workingDay: 'Full-time'
+  },
+  {
     id: '5615f938-8787-4211-9d2d-d188fb6b0a7c',
     position: 'Full-stack Developer',
     company: 'Biolink',
     location: 'USA',
     startDate: 'Agosto, 2022',
-    endDate: 'Presente',
+    endDate: 'Septiembre, 2024',
     achievements: [
       'Lidero el equipo de desarrollo como desarrollador Full-stack, utilizando el framework PHP Laravel para crear servicios API RESTful con base de datos MySQL, empleando el ORM Eloquent para garantizar una gestión eficiente de la base de datos.',
       'Implementé un microservicio de tracking con Node.js y NestJS, respaldado por una base de datos MongoDB, optimizando la escalabilidad y rendimiento del sistema.',
       'En el Frontend, utilicé React.js con Next.js, aprovechando el SSR (Server-Side Rendering) para mejorar el SEO y la velocidad de carga de la aplicación.',
       'Implementé Redux Toolkit y Context API para gestionar de manera eficiente el estado de la aplicación, haciendo uso de Hooks para la creación de componentes dinámicos.',
       'Integré la autenticación OAuth2 y JWT para garantizar la seguridad de las transacciones y el acceso autorizado a los recursos del sistema.',
-      'Incorporé Cookies e Internacionalización (Intl) para admitir 6 idiomas, mejorando la accesibilidad y la experiencia del usuario.',
+      'Incorporé Cookies e Internacionalización (Intl) para admitir 6 idiomas, mejorando la accesibilidad y la experiencia del usuario.'
     ],
-    workingDay: 'Full-time',
+    workingDay: 'Full-time'
   },
   {
     id: 'd46181a2-369b-4842-9d30-917bb88c833b',
@@ -32,9 +49,9 @@ export const experienceDB: ExperienceType[] = [
       'Implementé RabbitMQ y Redis para mejorar la gestión de mensajes y la eficiencia del sistema.',
       'Estrategias de seguridad con JWT, Passport y Spring Security.',
       'Aplicación de programación reactiva con RXJS y RxJava para mejorar la capacidad de respuesta.',
-      'Implementación de pruebas unitarias con Jest, Mockito, y monitoreo con NewRelic e integración para asegurar la calidad del software.',
+      'Implementación de pruebas unitarias con Jest, Mockito, y monitoreo con NewRelic e integración para asegurar la calidad del software.'
     ],
-    workingDay: 'Full-time',
+    workingDay: 'Full-time'
   },
   {
     id: '75d7cda0-54c7-459d-beb6-859a32a991a4',
@@ -51,9 +68,9 @@ export const experienceDB: ExperienceType[] = [
       'Utilicé JWT (JSON Web Tokens) y Passport para la autenticación segura de usuarios en el sistema.',
       'Implementé Redis para la gestión eficiente de caché y almacenamiento en memoria.',
       'Implementé React.js con Next.js para la creación de interfaces de usuario interactivas y modernas en el Frontend.',
-      'Colaboré en el diseño e implementación de estilos utilizando Tailwind CSS para una interfaz atractiva y responsiva.',
+      'Colaboré en el diseño e implementación de estilos utilizando Tailwind CSS para una interfaz atractiva y responsiva.'
     ],
-    workingDay: 'Full-time',
+    workingDay: 'Full-time'
   },
   {
     id: 'fd4b3413-409e-4863-93e8-f82e687d72d4',
@@ -66,8 +83,8 @@ export const experienceDB: ExperienceType[] = [
       'Desarrollé y mantuve el sistema de ventas de la empresa utilizando PowerBuilder, destacando en el mantenimiento de tablas maestras y generación de reportes.',
       'Brindé soporte remoto al usuario, garantizando una experiencia fluida y resolviendo eficientemente cualquier problema técnico relacionado con el sistema.',
       'Gestioné la Base de Datos en PostgreSql mediante PgAdmin, realizando consultas, funciones, reportes y actualización de datos para asegurar la integridad y eficiencia del sistema.',
-      'Contribuí activamente a la mejora continua del sistema, proponiendo y desarrollando soluciones para optimizar la funcionalidad y el rendimiento.',
+      'Contribuí activamente a la mejora continua del sistema, proponiendo y desarrollando soluciones para optimizar la funcionalidad y el rendimiento.'
     ],
-    workingDay: 'Full-time',
-  },
+    workingDay: 'Full-time'
+  }
 ];
--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -5,23 +5,14 @@ export const ProfileView = () => {
   const { name, lastName, bgUrl, level, photoUrl, status } = profileDB;
   return (
     <div className='card'>
-      <div
-        className='h-32 rounded-t-xl overflow-hidden bg-cover'
-        style={{ backgroundImage: `url(${bgUrl})` }}
-      >
+      <div className='h-32 rounded-t-xl overflow-hidden bg-cover' style={{ backgroundImage: `url(${bgUrl})` }}>
         <div className='bg-primary-600 dark:bg-gray-900 bg-opacity-60 dark:bg-opacity-70 h-full rounded-t-xl overflow-hidden p-2'>
           <DarkMode />
         </div>
       </div>
       <div className='pt-14 p-7 relative'>
-        <img
-          src={photoUrl}
-          alt='Gerard Ch.'
-          className='border-2 border-gray-100 dark:border-gray-700 rounded-xl w-20 h-20 absolute -top-10 shadow'
-        />
-        <span className='rounded-full px-2 py-1 bg-green-500 text-gray-100 text-xs font-semibold absolute top-4 right-7 uppercase'>
-          {status}
-        </span>
+        <img src={photoUrl} alt='Gerard Ch.' className='border-2 border-gray-100 dark:border-gray-700 rounded-xl w-20 h-20 absolute -top-10 shadow' />
+        <span className='rounded-full px-2 py-1 bg-green-500 text-gray-100 text-xs font-semibold absolute top-4 right-7 uppercase'>{status}</span>
         <div className='text-lg font-semibold mb-1.5'>
           {lastName} {name}
         </div>
@@ -35,17 +26,12 @@ export const ProfileView = () => {
             Contactar
           </a>
           <a
-            href='./cv-chaico-2024.pdf'
+            href='./cv-chaico.pdf'
             target='_blanck'
             className='text-gray-700 bg-slate-200 hover:bg-slate-300  p-3 rounded-r-lg dark:bg-slate-600 dark:hover:bg-slate-900 cursor-pointer dark:text-gray-200'
             title='Descargar CV'
           >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='currentColor'
-              className='w-6 h-6'
-              viewBox='0 0 24 24'
-            >
+            <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' className='w-6 h-6' viewBox='0 0 24 24'>
               <path d='m12 16 4-5h-3V4h-2v7H8z'></path>
               <path d='M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7z'></path>
             </svg>
